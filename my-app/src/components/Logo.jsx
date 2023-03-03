75e7142ac264e7412c1cf445ea9fbb9032406f61
cuckoodave <razer2807@gmail.com>
2023-03-03 15:30:11 +0100

    init

---
+++ my-app/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import logo from './logo1.png';
+export function Logo() {
+    return (
+      <figure>
+        <img src={logo} className="App-logo" alt="logo" />
+        <figcaption></figcaption>
+      </figure>
+    );
+  }
---
+++ my-app/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+export function Navigation() {
+    return (
+       <> 
+        <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <a href="1">test</a>
+      </>
+    );
+  }
+
+//<> </> mozemo dodati "fragment" da odvojimo element bez diva ili spana
